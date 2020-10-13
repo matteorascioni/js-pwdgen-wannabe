af4b40aa9e74ccc876a94f54200ebb76b784733e
Matteo <Rascioni>
2020-10-13 14:55:49 +0200

    Complete!

--- js/main.js
+++ js/main.js
@@ -1,7 +1,39 @@
-// RICHIESTA NOME UTENTE
+// USER NAME REQUEST
+var userName = prompt('Enter your name');
 
+document.getElementById('username').innerHTML = 'Name: ' + userName;
 
-// RICHIESTA COGNOME UTENTE
+// SECOND NAME REQUEST
+var surName = prompt('Enter your surname');
 
+document.getElementById('surname').innerHTML = 'Surname: ' + surName;
 
-// RICHIESTA COLORE PREFERITO UTENTE
+// YEARS AND AGE SECTION
+// CURRENTYEAR
+var annoAttuale = 2020;
+
+// AGE USER REQUEST
+var ageUser = prompt('Enter your age');
+
+document.getElementById('age').innerHTML = 'Age: ' + ageUser;
+
+// BORN YEAR
+var annoNascita = annoAttuale - ageUser;
+
+console.log(annoNascita);
+
+//USER CITY OF BIRTH
+var cityOfBirth = prompt('Enter your city of birth');
+
+document.getElementById('city').innerHTML = 'City of birth: ' + cityOfBirth; 
+
+// PHONE NUMBER
+var phoneNumber = prompt('Enter your phone number');
+
+document.getElementById('phone').innerHTML = 'Phone Number: ' + phoneNumber; 
+
+// STARRED COLOR
+var favouriteColor = prompt('Enter your favourite color');
+
+document.getElementById('favouritecolor').innerHTML = 'Favourite color: ' + favouriteColor;
+
